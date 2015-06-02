7e158da631847596ed753e295c04bb8be8f36c5e
Zachary Leighton <zleight1@gmail.com>
2015-06-01 21:14:36 -0400

    Initial navigation and some icons

--- ParveZman/ParveZman/AppDelegate.swift
+++ ParveZman/ParveZman/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert | .Badge | .Sound, categories: nil))  // types are UIUserNotificationType members
+        
         return true
     }
 
