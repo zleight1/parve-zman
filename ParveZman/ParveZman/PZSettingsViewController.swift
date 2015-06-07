c740085061f0fc66b0576d0b8555fe1a6815730b
Zachary Leighton <zleight1@gmail.com>
2015-06-07 13:29:50 -0400

    Fixing some navigation items

---
+++ ParveZman/ParveZman/PZSettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PZSettingsViewController.swift
+//  ParveZman
+//
+//  Created by Zachary Leighton on 6/7/15.
+//  Copyright (c) 2015 Zachary Leighton. All rights reserved.
+//
+
+import Cocoa
+
+class PZSettingsViewController: UIViewController {
+
+}
