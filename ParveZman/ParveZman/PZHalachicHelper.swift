8bac98242c5010866142cf3b1efee4b2befae493
Zachary Leighton <zleight1@gmail.com>
2015-06-18 19:31:03 -0400

    Bringing things up to speed

--- ParveZman/ParveZman/PZHalachicHelper.swift
+++ ParveZman/ParveZman/PZHalachicHelper.swift
@@ -21,20 +21,24 @@ class PZHalachicHelper {
     
     func updateHalachicTimesByLocation(latestLocation: CLLocation) {
         var location: KCGeoLocation = KCGeoLocation(latitude: latestLocation.coordinate.latitude, andLongitude: latestLocation.coordinate.longitude, andTimeZone: NSTimeZone.localTimeZone());
-        
+
         NSLog("%@", NSTimeZone.localTimeZone());
-        
+
         var calendar: KCAstronomicalCalendar = KCAstronomicalCalendar(location: location);
         var sunrise: NSDate! = calendar.sunrise();
         var sunset: NSDate! = calendar.sunset();
-        
-        NSLog("Sunrise %@ Sunset %@", calendar.sunrise(), calendar.sunset());
-        
-        
-        // var sunriseSet = EDSunriseSet.sunrisesetWithTimezone(NSTimeZone.localTimeZone(), latitude: latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude);
-        
-        var dayTimeInterval: NSTimeInterval = sunset.timeIntervalSinceDate(sunrise);
-        NSLog("Time Interval %@", dayTimeInterval);
 
+        NSLog("Sunrise %@ Sunset %@", calendar.sunrise(), calendar.sunset());
+
+
+        // var sunriseSet = EDSunriseSet.sunrisesetWithTimezone(NSTimeZone.localTimeZone(), latitude: latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude);
+
+        var dayTimeInterval: NSTimeInterval = sunset.timeIntervalSinceDate(sunrise);
+        NSLog("Time Interval %f", dayTimeInterval);
+
+        self.currentHalachicHour = CDouble(((dayTimeInterval/60)/60)/12);
+        
+        NSLog("Current Halachic Hour : %f in minutes : %f", self.currentHalachicHour, self.currentHalachicHour * 60);
+        
     }
 }
