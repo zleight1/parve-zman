d49d51bdc3a2ee6d9bfc3c7563f7e57e92b92d60
Zachary Leighton <zleight1@gmail.com>
2016-06-01 21:44:07 -0400

    Moved iOS into its own folder. Stubbed Android project.

--- ParveZman/Pods/Target Support Files/JTImageButton/JTImageButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JTImageButton : NSObject
-@end
-@implementation PodsDummy_JTImageButton
-@end
--- ParveZman/Pods/Target Support Files/JTImageButton/JTImageButton-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "JTImageButton.h"
-
-FOUNDATION_EXPORT double JTImageButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char JTImageButtonVersionString[];
-
