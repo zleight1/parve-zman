bf447f1038c5f94329431e41ded032d28980054f
Zachary Leighton <zleight1@gmail.com>
2016-06-15 22:13:50 -0400

    Added some more dimensions and added button handlers.

--- ParveZman-android/app/src/main/java/com/parvezman/www/parvezman/HomeActivity.java
+++ ParveZman-android/app/src/main/java/com/parvezman/www/parvezman/HomeActivity.java
@@ -2,6 +2,8 @@ package com.parvezman.www.parvezman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,7 +13,19 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-    protected void foo(){
-        ///
+    public void timerPressed(View view){
+        Log.d("timerPressed", "Timer was pressed.");
+        int id = view.getId();
+        if(id == R.id.meatButton){
+
+        } else {
+
+        }
     }
+
+    public void settingsPressed(View view){
+        Log.d("settingsPressed", "Settings was pressed.");
+    }
+
+
 }
