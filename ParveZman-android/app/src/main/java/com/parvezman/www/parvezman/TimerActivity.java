1db6f62f8be4d73ab6e213bd7271c7e191e063bf
Zachary Leighton <zleight1@gmail.com>
2016-07-22 11:01:20 -0400

    Timer text formatting
    Wrote formatting method to handle timer

--- ParveZman-android/app/src/main/java/com/parvezman/www/parvezman/TimerActivity.java
+++ ParveZman-android/app/src/main/java/com/parvezman/www/parvezman/TimerActivity.java
@@ -55,12 +55,12 @@ public class TimerActivity extends AppCompatActivity {
             }
         }
 
-        new CountDownTimer(30000, 100) {
+        new CountDownTimer(300000, 100) {
             ProgressWheel wheel = (ProgressWheel) findViewById(R.id.progress_wheel);
             TextView timerText = (TextView) findViewById(R.id.timerText);
             public void onTick(long millisUntilFinished) {
-                timerText.setText(String.valueOf(millisUntilFinished / 1000));
-                wheel.setInstantProgress((float)(30000 - millisUntilFinished)/(30000));
+                setTextTime(millisUntilFinished, timerText);
+                wheel.setInstantProgress((float)(300000 - millisUntilFinished)/(300000));
             }
 
             public void onFinish() {
@@ -68,6 +68,28 @@ public class TimerActivity extends AppCompatActivity {
                 wheel.setProgress(1.0f);
             }
         }.start();
+    }
+
+    private void setTextTime(long milliseconds, TextView timerText){
+        if(milliseconds <= 0){
+            return; //ignore it if we have no time left
+        }
+        long timeLeft = milliseconds / 1000;
+        int hours = (int) (timeLeft / 3600);
+
+        //subtract the hours
+        timeLeft -= hours * 3600.0;
+
+        int minutes = (int) (timeLeft / 60);
+
+        //subtract the minutes
+        timeLeft -= minutes * 60.0;
+
+        int seconds = (int) timeLeft;
+
+        String timeString = String.format("%02d:%02d:%02", hours, minutes, seconds);
+
+        timerText.setText(timeString);
     }
 
     @Override
